17de896c11430d233f7c7e7fc4fe3ac6ffeceb40
aqianglala <164748581@qq.com>
2016-03-05 16:44:06 +0800

    no message

--- app/src/main/java/com/example/admin/mygalleryview/fragment/ItemFragment.java
+++ app/src/main/java/com/example/admin/mygalleryview/fragment/ItemFragment.java
@@ -1,5 +1,9 @@
 package com.example.admin.mygalleryview.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,6 +26,7 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.example.admin.mygalleryview.R;
 import com.example.admin.mygalleryview.global.BaseFragment;
+import com.example.admin.mygalleryview.utils.L;
 import com.example.admin.mygalleryview.utils.ScreenUtils;
 
 import uk.co.senab.photoview.PhotoView;
@@ -51,18 +56,21 @@ public class ItemFragment extends BaseFragment{
         setContentView(R.layout.fragment_item);
         mPhotoView = getViewById(R.id.photo);
         mProgressBar = getViewById(R.id.progressbar);
+
+        mAttacher = new PhotoViewAttacher(mPhotoView);
     }
 
     @Override
     protected void setListener() {
-        mPhotoView.setOnClickListener(new View.OnClickListener() {
+        mAttacher.setOnViewTapListener(new PhotoViewAttacher.OnViewTapListener() {
             @Override
-            public void onClick(View v) {
+            public void onViewTap(View view, float x, float y) {
+                L.e("OnPhotoTapListener","x:"+x+" y:"+y);
                 mHandler.post(new Runnable() {
 
                     @Override
                     public void run() {
-                        enterAnimation();
+                        runExitAnimation();
                     }
                 });
             }
@@ -71,7 +79,6 @@ public class ItemFragment extends BaseFragment{
 
     @Override
     protected void processLogic(Bundle savedInstanceState) {
-        mAttacher = new PhotoViewAttacher(mPhotoView);
         // 获取数据
         getData();
         // 设置图片
@@ -115,20 +122,16 @@ public class ItemFragment extends BaseFragment{
         int endX = screenWidth / 2 - mWidth / 2;
         int endY =( screenHeight - ScreenUtils.getStatusBarHeight(mActivity))/ 2  - mHeight / 2 ;
 
-//        PropertyValuesHolder pvhX =PropertyValuesHolder.ofFloat("x",mPositionX,endX);
-//        PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat("y", mPositionY,endY);
-//        PropertyValuesHolder pvhAlpha = PropertyValuesHolder.ofFloat("alpha", 0.5f, 1f);
-//        PropertyValuesHolder pvhScaleX = PropertyValuesHolder.ofFloat("scaleX", 0f,1);
-//        PropertyValuesHolder pvhScaleY = PropertyValuesHolder.ofFloat("scaleY", 0f,1);
-
-        Animation translateAnimation = new TranslateAnimation(Animation.ABSOLUTE,mPositionX,Animation
+        Animation translateAnimation = new TranslateAnimation(Animation.ABSOLUTE,
+                mPositionX+mWidth/2,
+                Animation
                 .ABSOLUTE,endX,Animation
-                .ABSOLUTE,mPositionY,Animation.ABSOLUTE,endY);;
+                .ABSOLUTE,mPositionY+mHeight/2,Animation.ABSOLUTE,endY);;
 
         Animation scaleAnimation = new ScaleAnimation(0,1,0,1,0.5f,0.5f);
         AnimationSet animationSet = new AnimationSet(false);
-        animationSet.addAnimation(translateAnimation);
         animationSet.addAnimation(scaleAnimation);
+        animationSet.addAnimation(translateAnimation);
         animationSet.setDuration(300);
         animationSet.setFillAfter(true);
 
@@ -169,75 +172,78 @@ public class ItemFragment extends BaseFragment{
 
             }
         });
-//        final ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(mPhotoView,pvhScaleX,
-//                pvhScaleY, pvhX,
-//                pvhY).setDuration(300);
-
-//        animator.start();
-//        animator.addListener(new AnimatorListenerAdapter(){
-//            @Override
-//            public void onAnimationEnd(Animator animation) {
-//                mProgressBar.setVisibility(View.VISIBLE);
-//                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams
-//                        (RelativeLayout.LayoutParams
-//                                .MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-//                mPhotoView.setLayoutParams(params);
-//                Glide.with(ItemFragment.this).load(url).listener(new RequestListener<String, GlideDrawable>() {
-//
-//                    @Override
-//                    public boolean onException(Exception e, String model, Target<GlideDrawable>
-//                            target, boolean isFirstResource) {
-//                        return false;
-//                    }
-//
-//                    @Override
-//                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
-//                        mProgressBar.setVisibility(View.GONE);
-//                        mAttacher.setScaleType(ImageView.ScaleType.FIT_CENTER);
-//                        return (false);
-//                    }
-//                }).into(mPhotoView);
-//            }
-//        });
     }
 
     public void runExitAnimation(){
+//        BitmapDrawable bd= (BitmapDrawable) mPhotoView.getDrawable();
+//        Bitmap bitmap = bd.getBitmap();
+//
+//        BitmapFactory.Options options=new BitmapFactory.Options();
+//        options.inJustDecodeBounds=true;
+
         // 获取图片的宽高
         int measuredWidth = mPhotoView.getMeasuredWidth();
         int measuredHeight = mPhotoView.getMeasuredHeight();
-        //初始化
-        AnimationSet set = new AnimationSet(false);
 
-        Animation translateAnimation = new TranslateAnimation(Animation.ABSOLUTE,mImageX,Animation
-                .ABSOLUTE,mPositionX,Animation
-                .ABSOLUTE,mImageY,Animation.ABSOLUTE,mPositionY);
-        Animation scaleAnimation = new ScaleAnimation(mImageX,mPositionX,mImageY,mPositionY,Animation.RELATIVE_TO_SELF,0.5f,
-                Animation.RELATIVE_TO_SELF,0.5f);
-        //设置动画时间
-        set.addAnimation(translateAnimation);
-        set.addAnimation(scaleAnimation);
-        set.setDuration(1000);
-        set.setFillAfter(true);
+        // 获取起始位置
+        mPositionX = mParentFragment.getPositionX();
+        mPositionY = mParentFragment.getPositionY();
 
-        mPhotoView.startAnimation(set);
-        set.setAnimationListener(new Animation.AnimationListener() {
+//        Animation translateAnimation = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0,
+//                Animation.ABSOLUTE,mPositionX+mWidth/2,Animation
+//                .RELATIVE_TO_SELF,0,Animation.ABSOLUTE,mPositionY+mHeight/2);;
+//
+//        Animation scaleAnimation = new ScaleAnimation(1,mWidth/measuredWidth,1,mHeight/measuredHeight,Animation
+//                .RELATIVE_TO_SELF,0.5f,
+//                Animation.RELATIVE_TO_SELF,0.5f);
+//        AnimationSet animationSet = new AnimationSet(false);
+//        animationSet.addAnimation(scaleAnimation);
+//        animationSet.addAnimation(translateAnimation);
+//        animationSet.setDuration(300);
+//        animationSet.setFillAfter(true);
+
+        PropertyValuesHolder pvhX=PropertyValuesHolder.ofFloat("x",
+                0,mPositionX-(screenWidth/2-mWidth/2));
+        PropertyValuesHolder pvhY=PropertyValuesHolder.ofFloat("y",
+                0,mPositionY-(screenHeight/2-mHeight/2));
+        PropertyValuesHolder pvhScaleX=PropertyValuesHolder.ofFloat("scaleX",(float)
+                mWidth/measuredWidth);
+        PropertyValuesHolder pvhScaleY=PropertyValuesHolder.ofFloat("scaleY",(float)
+                mHeight/measuredHeight);
+
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(mPhotoView, pvhX,
+                pvhY, pvhScaleX, pvhScaleY);
+        objectAnimator.start();
+
+        objectAnimator.addListener(new AnimatorListenerAdapter() {
             @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
+            public void onAnimationEnd(Animator animation) {
                 FragmentManager manager = getActivity().getSupportFragmentManager();
                 FragmentTransaction trans = manager.beginTransaction();
                 manager.popBackStack();
             }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
         });
+
+//        mPhotoView.startAnimation(animationSet);
+//        animationSet.setAnimationListener(new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//                FragmentManager manager = getActivity().getSupportFragmentManager();
+//                FragmentTransaction trans = manager.beginTransaction();
+//                manager.popBackStack();
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//
+//            }
+//        });
     }
 
     private float mImageX;
@@ -246,7 +252,7 @@ public class ItemFragment extends BaseFragment{
         @Override
         public boolean onTouch(View v, MotionEvent event) {
             switch (event.getAction()){
-                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_DOWN:
                     mImageX = event.getRawX() - event.getX();
                     mImageY = event.getRawY() - event.getY();
                     break;
--- app/src/main/java/com/example/admin/mygalleryview/widget/MyViewpager.java
+++ app/src/main/java/com/example/admin/mygalleryview/widget/MyViewpager.java
@@ -1,7 +1,30 @@
 package com.example.admin.mygalleryview.widget;
 
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
 /**
  * Created by admin on 2016/3/5.
  */
-public class MyViewpager {
+public class MyViewpager extends ViewPager{
+
+    public MyViewpager(Context context) {
+        super(context);
+    }
+
+    public MyViewpager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
